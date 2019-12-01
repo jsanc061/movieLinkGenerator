8a0851387696db65780ea57b0e4747bfddde87ba
jsanc061 <sanchez.joshua777@gmail.com>
2019-12-01 02:20:58 -0600

    retrieves http link from general search

--- MovieLinkGenerator.java
+++ MovieLinkGenerator.java
@@ -1,6 +1,11 @@
 import okhttp3.*;
 import java.io.*;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 /**
  * This program reads a list of movies based on the Disney Plus listings,
  * parses it, and then generates a new list with links found to those movies.
@@ -17,18 +22,21 @@ public class MovieLinkGenerator {
     // reads the file with listed movies
     public void readMovieList() throws IOException {
 
-        File file = new File("src/DisneyMovies.txt");
+        File file = new File("src/DisneyMovies_Test.txt");
         BufferedReader br = new BufferedReader(new FileReader(file));
 
         String movieTitle;
 
         while( (movieTitle = br.readLine()) != null){
-            System.out.println(movieTitle);
+            String movieLink = searchMovie(movieTitle);
+            updateList(movieLink);
         }
     }
 
     // makes a POST request in the browser for the movie title
-    public void searchMovie(String movieTitle) throws IOException {
+    public String searchMovie(String movieTitle) throws IOException {
+
+        String movieLink;
 
         // form parameters
         RequestBody formBody = new FormBody.Builder()
@@ -36,7 +44,9 @@ public class MovieLinkGenerator {
                 .build();
 
         Request request = new Request.Builder()
-                .url("https://en.wikipedia.org/w/index.php")
+                .url("http://api.duckduckgo.com/?q="
+                        + "putlocker "
+                        + movieTitle)
                 //.url("https://www.bing.com/")
                 .addHeader("User-Agent", "OkHttp Bot")
                 .post(formBody)
@@ -49,21 +59,28 @@ public class MovieLinkGenerator {
             // Get response body
             //System.out.println(response.body().string());
 
-            System.out.println(response.toString());
+//            System.out.println(
+//                    request.url().toString());
+//                    response.toString());
+
+            movieLink = request.url().toString();
         }
+        System.out.println(movieLink);
+        return movieLink;
     }
 
     // writes the link found to a file
-    public void updateList(){
-
+    public void updateList(String movieLink) throws IOException {
+        FileWriter fileWriter = new FileWriter("src/Updated_DisneyMovies_Test.txt", true);
+        PrintWriter pw = new PrintWriter(fileWriter);
+        pw.println();
+        pw.println(movieLink);
+        pw.close();
     }
-
 
     public static void main(String[] args) throws IOException {
         MovieLinkGenerator mlv = new MovieLinkGenerator();
-
         System.out.println();
         mlv.readMovieList();
-        mlv.searchMovie("WALL-E");
     }
 }
